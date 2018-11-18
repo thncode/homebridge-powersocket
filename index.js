cc6e1e33996a36ee5e2b8946131cb6d0c241f09c
Thomas Nemec <30568242+thncode@users.noreply.github.com>
2018-11-18 10:05:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,46 +8,147 @@ module.exports = function (homebridge) {
     Characteristic = homebridge.hap.Characteristic;
     HomebridgeAPI = homebridge;
     FakeGatoHistoryService = require("fakegato-history")(homebridge);
-
+    	
     homebridge.registerAccessory("homebridge-powersocket", "PowerSocket", PowerSocketPlugin);
 };
 
 function PowerSocketPlugin(log, config) {
+	
+	var that = this;
+	this.log = log;
 
 	// Setup services
     this.setUpServices();
-};
-
-PowerSocketPlugin.prototype.getStatusLowBattery = function (callback) {
-    callback(null, Characteristic.StatusLowBattery.BATTERY_LEVEL_NORMAL);
+    
+    setInterval(function() {
+		// that.log("!");
+	}, 60000); 
 };
 
 PowerSocketPlugin.prototype.getFirmwareRevision = function (callback) {
-    callback(null, this.storedData.firmware ? this.storedData.firmware.firmwareVersion : '1.0.0');
+    callback(null, '1.0.0');
+};
+
+PowerSocketPlugin.prototype.getBatteryLevel = function (callback) {
+    callback(null, 100);
+};
+
+PowerSocketPlugin.prototype.getCurrentPowerConsumption = function (callback) {
+	callback(null, 2.3);
+};
+
+PowerSocketPlugin.prototype.getTotalConsumption = function (callback) {
+	callback(null, 20);
+};
+
+PowerSocketPlugin.prototype.setResetTotal = function (callback) {
+	callback(null, 0);
+};
+
+PowerSocketPlugin.prototype.getStatusActive = function (callback) {
+	callback(null, true);
 };
 
 PowerSocketPlugin.prototype.setUpServices = function () {
-    // info service
+
     this.informationService = new Service.AccessoryInformation();
 
     this.informationService
-        .setCharacteristic(Characteristic.Manufacturer, "THN") //this.config.manufacturer || "THN")
-        .setCharacteristic(Characteristic.Model, "PowerSocket") //this.config.model || "PowerSocket")
-        .setCharacteristic(Characteristic.SerialNumber, "0815") //this.config.serial || hostname + "-" + this.name);
+        .setCharacteristic(Characteristic.Manufacturer, "Thomas Nemec")
+        .setCharacteristic(Characteristic.Model, "PowerSocket")
+        .setCharacteristic(Characteristic.SerialNumber, "0815");
     this.informationService.getCharacteristic(Characteristic.FirmwareRevision)
         .on('get', this.getFirmwareRevision.bind(this));
 
-    this.batteryService = new Service.BatteryService(this.name);
-    this.batteryService.getCharacteristic(Characteristic.BatteryLevel)
-        .on('get', this.getBatteryLevel.bind(this));
-    this.batteryService.setCharacteristic(Characteristic.ChargingState, Characteristic.ChargingState.NOT_CHARGEABLE);
-    this.batteryService.getCharacteristic(Characteristic.StatusLowBattery)
-        .on('get', this.getStatusLowBattery.bind(this));
+    this.fakeGatoHistoryService = new FakeGatoHistoryService("energy", this, { storage: 'fs' });
+    
+	CurrentPowerConsumption = function () {
+		Characteristic.call(this, 'Verbrauch', 'E863F10D-079E-48FF-8F27-9C2605A29F52');
+		this.setProps({
+			format: Characteristic.Formats.UINT16,
+			unit: "Watt",
+			maxValue: 100000,
+			minValue: 0,
+			minStep: 1,
+			perms: [Characteristic.Perms.READ, Characteristic.Perms.NOTIFY]
+		});
+		this.value = this.getDefaultValue();
+	};
+
+	CurrentPowerConsumption.UUID = 'E863F10D-079E-48FF-8F27-9C2605A29F52';
+	inherits(CurrentPowerConsumption, Characteristic);
+
+	TotalConsumption = function () {
+		Characteristic.call(this, 'Leistung', 'E863F10C-079E-48FF-8F27-9C2605A29F52');
+		this.setProps({
+			format: Characteristic.Formats.FLOAT,
+			unit: "kWh",
+			maxValue: 100000000000,
+			minValue: 0,
+			minStep: 0.001,
+			perms: [Characteristic.Perms.READ, Characteristic.Perms.NOTIFY]
+		});
+		this.value = this.getDefaultValue();
+	};
+	TotalConsumption.UUID = 'E863F10C-079E-48FF-8F27-9C2605A29F52';
+	inherits(TotalConsumption, Characteristic);
+
+	ResetTotal = function () {
+		Characteristic.call(this, 'Verbrauch zurücksetzen', 'E863F112-079E-48FF-8F27-9C2605A29F52');
+		this.setProps({
+			format: Characteristic.Formats.UINT32,
+			perms: [Characteristic.Perms.READ, Characteristic.Perms.NOTIFY, Characteristic.Perms.WRITE]
+		});
+		this.value = this.getDefaultValue();
+	};
+	ResetTotal.UUID = 'E863F112-079E-48FF-8F27-9C2605A29F52';
+	inherits(ResetTotal, Characteristic);
+/*
+    PowerSocket = function (displayName, subtype) {
+		Service.call(this, displayName, '0000003E-0000-1000-8000-0026BB765291', subtype);
+		
+		this.addCharacteristic(CurrentPowerConsumption);
+		this.addCharacteristic(TotalConsumption);
+		this.addCharacteristic(ResetTotal);
+	};
+	
+	inherits(PowerSocket, Service);
+	
+	PowerSocket.UUID = 'd2af0189-4086-4788-9030-edca267ed4ed';
+        
+    this.powerSocketService = new PowerSocket(this.name);
+    
+	this.powerSocketService.getCharacteristic(Characteristic.StatusActive)
+		.on('get', this.getStatusActive.bind(this));
+*/
+    this.service = new Service.Switch(this.name);
+    
+    this.service
+		.getCharacteristic(Characteristic.On)
+		.on('get', this.getOn.bind(this))
+		.on('set', this.setOn.bind(this));
+		
+	this.service.getCharacteristic(CurrentPowerConsumption)
+		.on('get', this.getCurrentPowerConsumption.bind(this));
+		
+	this.service.getCharacteristic(TotalConsumption)
+		.on('get', this.getTotalConsumption.bind(this));
+
+	this.service.getCharacteristic(ResetTotal)
+		.on('get', this.setResetTotal.bind(this));
 };
+
+PowerSocketPlugin.prototype.getOn = function(callback) {
+	callback(null, true);
+}
+
+PowerSocketPlugin.prototype.setOn = function(on, callback) {
+	callback(null);
+}
 
 PowerSocketPlugin.prototype.getServices = function () {
 	
-    var services = [this.informationService, this.batteryService];
+    var services = [this.informationService, this.service, this.fakeGatoHistoryService];
 
     return services;
 };
